716e86bf707ad0d97c9a4570c719b457989b46f3
Ahmed Gamal <113640948+ahmedgamal1200@users.noreply.github.com>
2025-03-02 16:54:54 +0200

    added table sections, template, section_template and installed filament 3.3

---
+++ database/migrations/2025_03_02_135907_create_sections_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use App\Models\Section;
+use App\Models\Template;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sections', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique()->index();
+            $table->jsonb('data')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('templates', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique()->index();
+            $table->timestamps();
+        });
+
+        Schema::create('section_template', function (Blueprint $table) {
+           $table->foreignIdFor(Section::class)
+               ->constrained('sections')
+               ->cascadeOnDelete();
+            $table->foreignIdFor(Template::class)
+                ->constrained('templates')
+                ->cascadeOnDelete();
+            $table->unsignedInteger('order');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sections');
+        Schema::dropIfExists('template');
+        Schema::dropIfExists('section_template');
+    }
+};
